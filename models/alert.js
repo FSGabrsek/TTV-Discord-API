7236e4c7911e0ebee27651e5c35eb09c6656ef81
FSGabrsek <f.gabrsek@gmail.com>
2023-01-26 20:13:08 +0100

    added webhooks array to alert schema

--- models/alert.js
+++ models/alert.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const discordWebHook = new Schema({
+    content: {
+        type: String,
+        required: [ true, 'discordWebHook property "content" is required' ]
+    },
+    url: {
+        type: String,
+        required: [ true, 'discordWebHook property "url" is required']
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: [ true, 'discordWebHook property "owner" is required']
+    }
+})
+
 const schema = new Schema({
     eventsub_id: {
         type: String,
         required: [ true, 'Subscription property "eventsub_id" is required' ]
     },
-    content: {
-        type: String,
-        required: [ true, 'Subscription property "message" is required' ]
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: [ true, 'Subscription property "owner" is required']
-    }
+    discord_webhooks: [discordWebHook]
 });
 
 const Alert = mongoose.model('alert', schema);
